379845c82aeadc4f237d3fb44c96bb456e33d5a3
fnzl54 <rnjscksdud1@naver.com>
2024-10-11 18:16:41 +0900

    feat: spring, git 초기 셋팅 (NONE)

---
+++ src/main/java/lucas/spring_laboratory/SpringLaboratoryApplication.java
@@ -0,0 +1,13 @@
+package lucas.spring_laboratory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLaboratoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLaboratoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lucas/spring_laboratory/SpringLaboratoryApplicationTests.java
@@ -0,0 +1,13 @@
+package lucas.spring_laboratory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLaboratoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
