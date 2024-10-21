a627e38b918d3686c19fc52f156b33c8469aea8a
fnzl54 <rnjscksdud1@naver.com>
2024-10-21 17:28:32 +0900

    test: fetch join 사용 시 pagination 문제 (#1)

--- src/main/java/lucas/spring_laboratory/core/domain/repository/UserJPARepository.java
+++ src/main/java/lucas/spring_laboratory/core/domain/repository/UserJPARepository.java
@@ -3,6 +3,8 @@ package lucas.spring_laboratory.core.domain.repository;
 import java.util.List;
 import java.util.UUID;
 import lucas.spring_laboratory.core.domain.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -15,4 +17,7 @@ public interface UserJPARepository extends JpaRepository<User, UUID> {
 
   @Query("select distinct u from User u left join fetch u.posts")
   List<User> findAllFetchJoin();
+
+  @Query("select distinct u from User u left join fetch u.posts")
+  Page<User> findAllFetchJoinPage(Pageable pageable);
 }
